895b319cc06fd155af3e8c18cb2de576acd1e003
B0calon <pedrobocalon98@gmail.com>
2025-12-12 18:32:13 -0300

    Update Shadow.vs.glsl
    adição nova sombra

--- Shadow Mapping/shaders/Shadow.vs.glsl
+++ Shadow Mapping/shaders/Shadow.vs.glsl
@@ -6,14 +6,18 @@ uniform mat4 mWorld;
 
 attribute vec3 vPos;
 attribute vec3 vNorm;
+attribute vec2 vTexCoord; // <--- O Javascript PRECISA enviar isso
 
 varying vec3 fPos;
 varying vec3 fNorm;
+varying vec2 fTexCoord;   // <--- Envia para o Fragment Shader
 
 void main()
 {
 	fPos = (mWorld * vec4(vPos, 1.0)).xyz;
 	fNorm = (mWorld * vec4(vNorm, 0.0)).xyz;
-
+	
+	fTexCoord = vTexCoord; // Passa a coordenada adiante
+	
 	gl_Position = mProj * mView * vec4(fPos, 1.0);
 }
